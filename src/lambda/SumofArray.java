b59865477a27329a835c2cad232b6a93fdd8c9ff
sabri baazaoui <baazaouisabri@gmail.com>
2020-10-31 21:51:49 +0100

    Initial commit

---
+++ src/lambda/SumofArray.java
@@ -0,0 +1,44 @@
+package lambda;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.IntStream;
+
+
+public class SumofArray {
+    public static int [] array = IntStream.rangeClosed(0,500).toArray();
+    public static int totlal = IntStream.rangeClosed(0,500).sum();
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        Callable callable = () -> {
+
+            int sum=0;
+            for (int i=0; i< array.length/2; i++ ){
+                sum = sum + array[i];
+            }
+      return sum;
+        };
+
+        Callable callable2 = () -> {
+
+            int sum=0;
+            for (int i=array.length/2; i< array.length;i++){
+                sum = sum + array[i];
+            }
+            return sum;
+        };
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        List<Callable<Integer>> task = Arrays.asList(callable,callable2);
+        List<Future<Integer>> results = executorService.invokeAll(task);
+       int k=0;
+       int sum = 0;
+       for (Future<Integer> result:results){
+           sum = sum+result.get();
+           System.out.println(" sum of thread "+ ++k +" is "+sum);
+       }
+        System.out.println(sum);
+        System.out.println(totlal);
+        executorService.shutdown();
+    }
+}
